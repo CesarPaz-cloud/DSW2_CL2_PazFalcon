f59de0370095b3c8fc0863bcdc1d34eb043914a4
CesarPaz-cloud <cesarpazfalcon1@gmail.com>
2023-10-28 22:01:37 -0500

    Primer Commit

---
+++ src/main/java/pe/edu/cibertec/DSW2_CL2_PazFalcon/Dsw2Cl2PazFalconApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSW2_CL2_PazFalcon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsw2Cl2PazFalconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsw2Cl2PazFalconApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DSW2_CL2_PazFalcon/Dsw2Cl2PazFalconApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSW2_CL2_PazFalcon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsw2Cl2PazFalconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
